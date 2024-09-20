615a93c3d441644d2c2edb030bc5338faf66dcca
Rafael Correa <rafaelscorreadev@gmail.com>
2024-09-19 22:36:48 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
+    });
+
+    $('.menu-hamburguer').click(function() {
+        $('nav').slideToggle();
+    });
+
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: { 
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            veiculoDeInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form) {
+            console.log(form);
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos.`);
+            }
+        }
+    });
+
+    $('.lista-veiculos button').click(function() { // Correção aqui
+        const destino = $('#contato');
+
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculo-interesse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000);
+    });
+});
